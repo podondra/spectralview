03a3e39ce3401b4257a5a053f554617cac04ccb4
Ondrej Podsztavek <ondrej.podsztavek@gmail.com>
2017-01-29 20:37:34 +0100

    Add basic auth

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 import io
 import json
 import tornado.ioloop
@@ -19,6 +20,9 @@ class BaseHandler(tornado.web.RequestHandler):
     def db(self):
         return self.application.db
 
+    def get_current_user(self):
+        return self.get_secure_cookie('user')
+
 
 class FigureHandler(BaseHandler):
     async def get(self, spectrum_id):
@@ -33,7 +37,7 @@ class FigureHandler(BaseHandler):
         self.render('figure.html', host=self.request.host, fig_num=fig_num)
 
 
-class MplJsHandler(tornado.web.RequestHandler):
+class MplJsHandler(BaseHandler):
     def get(self):
         self.set_header('Content-Type', 'application/javascript')
         js = FigureManagerWebAgg.get_javascript()
@@ -79,11 +83,34 @@ class SpectraHandler(BaseHandler):
             spectra.append(spectrum)
         self.render('show_spectra.html', spectra=spectra)
 
+    @tornado.web.authenticated
     async def post(self):
         data = parse_fits(io.BytesIO(self.request.files['file'][0]['body']))
         await self.db.spectra.insert_one(data)
         self.redirect('/')
 
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        self.render('login.html', error=None)
+
+    async def post(self):
+        username = self.get_argument('username')
+        user = await self.db.users.find_one({'username': username})
+        if not user:
+            self.render('login.html', error='user not found')
+            return
+        if self.get_argument('password') == user['password']:
+            self.set_secure_cookie('user', username)
+            self.redirect('/')
+        else:
+            self.render('login.html', error='incorrect password')
+
+
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie('user')
+        self.redirect('/')
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -92,14 +119,19 @@ class Application(tornado.web.Application):
                 (r'/spectra/([0-9a-z]+)', FigureHandler),
                 (r'/mpl.js', MplJsHandler),
                 (r'/([0-9]+)/ws', WebSocketHandler),
+                (r'/login', LoginHandler),
+                (r'/logout', LogoutHandler),
                 ]
         setting = dict(
                 template_path=os.path.join(os.path.dirname(__file__), 'templates'),
                 debug=True,
+                login_url='/login',
+                cookie_secret=str(uuid.uuid4()),
                 )
         super(Application, self).__init__(handlers, **setting)
 
         self.db = motor.MotorClient('172.17.0.2', 27017).test
+        self.db.users.insert_one({'username': 'admin', 'password': 'default'})
 
 
 if __name__ == '__main__':
